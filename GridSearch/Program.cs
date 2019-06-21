19890044473ef0a2de57f06de17c67d59dcb55a1
Cornelius Wiens <cmw1334@gmail.com>
2019-06-20 20:40:39 -0500

    Initial commit of project

---
+++ GridSearch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GridSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int colCount = 10;
+            int rowCount = 3;
+
+            for (int x = 0; x < colCount; x++)
+            {
+                Console.WriteLine($"Column:{x + 1}");
+            }
+        }
+    }
+}
